7926e14e3d947541ba9fb6548b576c9331791e2e
Linam1210 <linamosquera1@linas-mbp.gateway.pace.com>
2015-07-09 18:02:29 -0500

    variables

--- Mosquera_Lina_Output_Assignment/js/Mosquera_Lina_Output_Assignment.js
+++ Mosquera_Lina_Output_Assignment/js/Mosquera_Lina_Output_Assignment.js
@@ -7,8 +7,8 @@
 var myNationality = "colombian";
 var favoriteFood = "seafood";
 
-console.log("Hi, my nationality is " + myNationality)
-console.log("My Favorite Food is " + favoriteFood)
+console.log("Hi, my nationality is " + myNationality);
+console.log("My Favorite Food is " + favoriteFood);
 
 //assigning number variables//
 
@@ -16,16 +16,16 @@ var myPhoneNumber = 2817303338;
 var numberOfSiblings = 2;
 
 console.log("you can always reach me at "
-+ myPhoneNumber)
-console.log("I have " + numberOfSiblings + " siblings " )
++ myPhoneNumber);
+console.log("I have " + numberOfSiblings + " siblings " );
 
 //assigning Boolean variable//
 
 var isMarried = false;
 var anyPets = true;
 
-console.log("I am married." + isMarried)
-console.log("I have pets." +anyPets)
+console.log("I am married." + isMarried);
+console.log("I have pets." +anyPets);
 
 
 
