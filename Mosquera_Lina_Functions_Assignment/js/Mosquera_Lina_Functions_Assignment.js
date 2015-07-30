b4bc55f8c64ea87e4493a98b2159abf41b2d8d6b
Linam1210 <linamosquera1@linas-mbp.gateway.pace.com>
2015-07-29 21:38:41 -0500

    setting up functions

--- Mosquera_Lina_Functions_Assignment/js/Mosquera_Lina_Functions_Assignment.js
+++ Mosquera_Lina_Functions_Assignment/js/Mosquera_Lina_Functions_Assignment.js
@@ -4,3 +4,15 @@
 
 var lottery = prompt("Please enter 1 if you would like number for the Florida Lottery or 2 if you would like number fo the Power Ball Lottery:")// this variable is to ask the user to choose which lottery they want numbers for//
 
+function lotteryVal(lotterySelec) { //here I am setting up the first function to validate the users input//
+
+    while (lotterySelec === "") { //By using the while loop here we make sure that the user selects one of the two lotteries//
+
+        lotterySelec = prompt("Please choose 1 for the Florida Lottery or 2 for the Power Ball Lottery:")// asking the user to enter a selection//
+    }
+
+
+    return lotterySelec;// I am returning the value entered by the user//
+
+
+}
