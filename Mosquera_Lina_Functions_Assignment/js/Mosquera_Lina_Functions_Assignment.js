bacfd312ffd35e033e63156ace0f45586ac9aa0e
Linam1210 <linamosquera1@linas-mbp.gateway.pace.com>
2015-07-29 21:49:24 -0500

    setting up last function

--- Mosquera_Lina_Functions_Assignment/js/Mosquera_Lina_Functions_Assignment.js
+++ Mosquera_Lina_Functions_Assignment/js/Mosquera_Lina_Functions_Assignment.js
@@ -2,13 +2,13 @@
 //SDI 1507
 //Functions Assignment
 
-var lottery = prompt("Please enter 1 if you would like number for the Florida Lottery or 2 if you would like number fo the Power Ball Lottery:")// this variable is to ask the user to choose which lottery they want numbers for//
+var lottery = prompt("Please enter 1 if you would like number for the Florida Lottery or 2                if you would like number fo the Power Ball Lottery:"); // this variable is to               ask the user to choose which lottery they want numbers for//
 
 function lotteryVal(lotterySelec) { //here I am setting up the first function to validate the users input//
 
-    while (lotterySelec === "") { //By using the while loop here we make sure that the user selects one of the two lotteries//
+    while (lotterySelec === "") { //By using the while loop here we make sure that the user     selects one of the two lotteries//
 
-        lotterySelec = prompt("Please choose 1 for the Florida Lottery or 2 for the Power Ball Lottery:")// asking the user to enter a selection//
+        lotterySelec = prompt("Please choose 1 for the Florida Lottery or 2 for the Power           Ball Lottery:"); // asking the user to enter a selection//
     }
 
 
@@ -27,11 +27,29 @@ function floridaLott (min, max, num) { // this function is set up for the Florid
         for (var i = 0; i < num; i++) { // for loop is set up to generate random numbers            inside of the array
 
             var floridaRandom = Math.random() * (max - min) + min;
-            floridaArray [i] = Math.round(floridaRandom)// math object here is use to
+            floridaArray [i] = Math.round(floridaRandom); // math object here is use to
             //generate random numbers and also to have those numbers rounded to a whole                 number//
 
         }
-        return floridaArray;// returning the random number into the array variable//
+        return floridaArray; // returning the random number into the array variable//
+    }
+
+}
+
+function powerBallLott (min, max, num){ //function shet up for the Power Ball Lottery//
+
+    if (lottery === "2"){ // conditional set if the user selects option 2 for the Power Ball    Lottery//
+
+        var powerBArray = []; // Array variable is to hold the random numbers for the Power         Ball Lottery//
+
+        for (var i = 0; i < num; i++){ // for loop is set up to generate random numbers            inside of the array
+            var powerBRandom = Math.random() * (max - min) + min;
+            powerBArray [i] = Math.round(powerBRandom); // math object here is use to
+            //generate random numbers and also to have those numbers rounded to a whole                 number//
+        }
+
+        return powerBArray; // returning the random number into the array variable//
+
     }
 
 }
