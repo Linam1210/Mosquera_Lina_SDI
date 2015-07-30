79d32edeed14dd7778721744fa5086f53630efab
Linam1210 <linamosquera1@linas-mbp.gateway.pace.com>
2015-07-29 21:35:35 -0500

    Declaring variables

---
+++ Mosquera_Lina_Conditional_Assignment/js/Mosquera_lina_conditionals_assignment.js
@@ -0,0 +1,7 @@
+/* Lina Mosquera
+SDI 201507
+Conditionals assignment
+ */
+
+// How much it will cost to go to vacation to Cancun, Mexico//
+
---
+++ Mosquera_Lina_Functions_Assignment/js/Mosquera_Lina_Functions_Assignment.js
@@ -0,0 +1,6 @@
+//Lina Mosquera
+//SDI 1507
+//Functions Assignment
+
+var lottery = prompt("Please enter 1 if you would like number for the Florida Lottery or 2 if you would like number fo the Power Ball Lottery:")// this variable is to ask the user to choose which lottery they want numbers for//
+
